b50da6c62eca1fa8f5e68e6d1181c32e5d745711
kikoriddle <zhuzeng@usc.edu>
2024-11-19 22:56:05 -0800

    uPDAER

--- Assets/Script/IconManager - Copy.cs
+++ Assets/Script/IconManager - Copy.cs
@@ -1,33 +1,15 @@
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class IconManager : MonoBehaviour
 {
     // Hover settings
     private SpriteRenderer spriteRenderer;
-  
-
-    void Start()
-    {
-     
-    }
-
-    // Mouse hover effects
-    void OnMouseEnter()
-    {
-        
-    }
-
-    void OnMouseExit()
-    {
-       
-    }
 
     void OnMouseDown()
     {
         string sceneName = gameObject.tag; // Use the tag name directly as the scene name
         Debug.Log($"Mouse clicked on icon with tag: {sceneName}");
-
         if (!string.IsNullOrEmpty(sceneName))
         {
             LoadScene(sceneName);
@@ -37,7 +19,6 @@ public class IconManager : MonoBehaviour
             Debug.LogError("Scene name is empty or not assigned!");
         }
     }
-
     private void LoadScene(string sceneName)
     {
         Debug.Log($"Loading scene: {sceneName}");
